710156441de818dfccfedd87018026d79fc30dcb
rohithdepa <rohithreddydepa>
2022-06-06 18:45:45 +0530

    added side bar and routerlinks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnalysisComponent } from './analysis/analysis.component';
+import { IntoductionComponent } from './intoduction/intoduction.component';
+import { PredictComponent } from './predict/predict.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IntoductionComponent },
+  { path: 'predict', component: PredictComponent },
+  { path: 'analysis', component: AnalysisComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SiderBarComponent } from './sider-bar/sider-bar.component';
 
+import { CardModule } from 'primeng/card';
+import { SkeletonModule } from 'primeng/skeleton';
+import { IntoductionComponent } from './intoduction/intoduction.component';
+import { PredictComponent } from './predict/predict.component';
+import { AnalysisComponent } from './analysis/analysis.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SiderBarComponent, IntoductionComponent, PredictComponent, AnalysisComponent],
+  imports: [BrowserModule, AppRoutingModule, CardModule, SkeletonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
