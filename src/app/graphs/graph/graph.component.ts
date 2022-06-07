606ef9d6f9dc97410f48a812be79d1d97312e2eb
rohithdepa <rohithreddydepa>
2022-06-07 22:25:51 +0530

    graphs and predict is added

---
+++ src/app/graphs/graph/graph.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { HttpService } from 'src/app/http.service';
+import { graph } from 'src/app/models';
+
+@Component({
+  selector: 'app-graph',
+  templateUrl: './graph.component.html',
+  styleUrls: ['./graph.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class GraphComponent implements OnInit {
+  @Input() type: string = '';
+  isLoading = false;
+  data: graph = {} as any;
+  constructor(public http: HttpService) {}
+  ngOnInit() {
+    this.isLoading = true;
+    this.getData();
+  }
+  getData() {
+    this.isLoading = true;
+    this.http.get(`/graphs`, { params: { type: this.type } }).subscribe({
+      next: (response: any) => {
+        this.isLoading = false;
+        this.data = response;
+        console.log(this.data);
+      },
+      error: (err) => {
+        this.isLoading = false;
+        console.log('error');
+      },
+    });
+  }
+}
--- src/app/graphs/graphs.component.ts
+++ src/app/graphs/graphs.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-graphs',
   templateUrl: './graphs.component.html',
-  styleUrls: ['./graphs.component.scss']
+  styleUrls: ['./graphs.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class GraphsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  ngOnInit() {}
+  graphs = ['F1', 'Precision', 'Recall'];
 }
