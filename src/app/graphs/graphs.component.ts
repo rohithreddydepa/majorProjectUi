dfde315006446d64aaa72cff3709f22fd0da92c1
rohithdepa <rohithreddydepa>
2022-06-20 20:27:15 +0530

    error msg changed and added text

--- src/app/graphs/graphs.component.ts
+++ src/app/graphs/graphs.component.ts
@@ -11,7 +11,7 @@ export class GraphsComponent implements OnInit {
   ngOnInit() {}
   graphs = ['F1', 'Precision', 'Recall'];
   data = [
-    'F1Score: This score will give us the harmonic mean of precision and recall. Mathematically, F1 score is the weighted average of the precision and recall. The best value of F1 would be 1 and worst would be 0.',
+    'F1Score: The F1 score is a machine learning metric that can be used in classification models.Precision and Recall are the two most common metrics that take into account class imbalance They are also the foundation of the F1 score. Mathematically, F1 score is the weighted average of the precision and recall. The best value of F1 would be 1 and worst would be 0.',
     'Precision: To overcome the limitations of Accuracy, Data Scientists usually use Precision, Recall and Specificity. Precision tells what proportion of positive predictions was actually correct. It achieves this by counting the samples correctly predicted as positive (TP) and dividing it by the total positive predictions, correct or incorrect (TP, FP).',
     'Recall: Recall aims at measuring what proportion of actual positives was identified correctly. It does so by dividing the correctly predicted positive samples (TP) by the total number of positives, either correctly predicted as positive or incorrectly predicted as negative (TP, FN).',
   ];
