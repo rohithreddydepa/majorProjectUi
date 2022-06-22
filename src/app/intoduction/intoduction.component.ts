1a49ab382db4e6267afbb2c57e1f905b9c93901e
aishwaryadevi4 <55918658+aishwaryadevi4@users.noreply.github.com>
2022-06-23 01:10:09 +0530

    MinorChangesInUI

--- src/app/intoduction/intoduction.component.ts
+++ src/app/intoduction/intoduction.component.ts
@@ -26,7 +26,7 @@ export class IntoductionComponent implements OnInit {
         icon: PrimeIcons.CIRCLE,
         date: '',
         showMore: true,
-        data: 'Data preprocessing is the first and foremost step before feeding the data to the machine learning model.Because the algorithms learn from the data and it is heavily depends on it. Some of the data preprocessing steps we used are removing html tags and punctuation marks, converting all letters to lower case etc. for the better prediction of tags.',
+        data: 'Data preprocessing is the first and foremost step before feeding the data to the machine learning model.Because the algorithms learn from the data and it heavily depends on it. Some of the data preprocessing steps we used are removing html tags and punctuation marks, converting all letters to lower case etc. for the better prediction of tags.',
       },
       {
         status: 'Training the model',
@@ -34,7 +34,7 @@ export class IntoductionComponent implements OnInit {
         icon: PrimeIcons.CIRCLE,
         date: '',
         showMore: true,
-        data: 'This step is the crucial in the project. The model training step involves exploring, analysing and engineering new features, splitting dataset, making the choice for the most suitable algorithm and then validating the created model for the prediction. We have split the dataset 80-20 to train and test the model and decided to go with the OneVsRest classifier algorithm of LinearSVC owing to the size and complexity of the dataset.',
+        data: 'This step is crucial in the project. The model training step involves exploring, analysing and engineering new features, splitting dataset, making the choice for the most suitable algorithm and then validating the created model for the prediction. We have split the dataset 75-25 to train and test the model and decided to go with the OneVsRest classifier algorithm of LinearSVC owing to the size and complexity of the dataset.',
       },
       {
         status: 'Result analysis',
@@ -42,7 +42,7 @@ export class IntoductionComponent implements OnInit {
         icon: PrimeIcons.CIRCLE,
         date: '',
         showMore: true,
-        data: 'This step is important to interpret the results achieved. The various performance measures of Machine Learning like F1 Score, Precision, Recall and Hamming Loss(HL) have been studied. These performance measures have been compared for the 3 different categories-Title based prediction, Body based prediction and Body and Title based prediction. Graphs are displayed for easy visual interpretation. As the performance of model developed based on both the title and body is best we built the model based on it ',
+        data: 'This step is important to interpret the results achieved. The various performance measures of Machine Learning like F1 Score, Precision, Recall, Accuracy and Hamming Loss(HL) have been studied. These performance measures have been compared for the 3 different categories-Title based prediction, Body based prediction and Body and Title based prediction. Graphs are displayed for easy visual interpretation. We have currently built the model based on title and body together.',
       },
       {
         status: 'User Interface',
@@ -50,7 +50,7 @@ export class IntoductionComponent implements OnInit {
         icon: PrimeIcons.CIRCLE,
         date: '',
         showMore: true,
-        data: 'Ease of use of the given model is equally important and one of the major requirements. We have used various front end technologies like Angular, HTML, CSS, Bootstrap etc to improve the user interface. This step is quite necessary as we need to predict and display the results to the user in as simple as possible',
+        data: 'Ease of use of the given model is equally important and one of the major requirements. We have used various front end technologies like Angular, HTML, CSS, Bootstrap etc to improve the user interface. This step is quite necessary as we need to predict and display the results to the user in as simple as possible manner.',
       },
     ];
   }
